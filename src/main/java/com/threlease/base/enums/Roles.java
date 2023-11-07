5d54367bb261f67a43213b92b362f55573e8daee
cth.release <84012697+th-release@users.noreply.github.com>
2023-11-07 09:10:27 +0900

    Update Roles.java

--- src/main/java/com/threlease/base/enums/Roles.java
+++ src/main/java/com/threlease/base/enums/Roles.java
@@ -1,4 +1,4 @@
-package com.threlease.base.enums;
+package com.nullvariable.devlog.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -6,5 +6,5 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Roles {
-    USER, ADMIN
+    ROLE_USER, ROLE_ADMIN
 }
