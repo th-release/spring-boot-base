fe3ee5fe34d94a4904b0dd913a725a7c2049f8c2
th-release <cth.release@gmail.com>
2024-03-18 09:17:17 +0900

    feat: Added Pagination

--- src/main/java/com/threlease/base/repositories/AuthRepository.java
+++ src/main/java/com/threlease/base/repositories/AuthRepository.java
@@ -1,6 +1,8 @@
 package com.threlease.base.repositories;
 
 import com.threlease.base.entites.AuthEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,6 +19,6 @@ public interface AuthRepository extends JpaRepository<AuthEntity, String> {
     @Query(value = "SELECT u FROM AuthEntity u WHERE u.username = :username")
     Optional<AuthEntity> findOneByUsername(@Param("username") String username);
 
-    @Query("SELECT u FROM AuthEntity u ORDER BY u.createdAt DESC LIMIT :limit")
-    List<AuthEntity> findAllLimitOrderByCreatedAtDesc(@Param("limit") int limit);
+    @Query(value = "SELECT u FROM AuthEntity u")
+    Page<AuthEntity> findByPagination(Pageable pageable);
 }
