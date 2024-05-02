e3267de0bffe5a4c723128b4c678f34e0580ddb0
th-release <cth.release@gmail.com>
2024-05-02 20:10:14 +0900

    feat: Added swagger

--- src/main/java/com/threlease/base/utils/security/WebSecurityConfig.java
+++ src/main/java/com/threlease/base/utils/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.threlease.base.utils.security;
 
-import com.threlease.base.enums.Roles;
 import com.threlease.base.utils.jsonwebtoken.JwtAuthenticationFilter;
 import com.threlease.base.utils.jsonwebtoken.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -28,35 +27,19 @@ public class WebSecurityConfig {
                         management.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/community/**").hasRole("USER")
+                        // swagger
+                        .requestMatchers("/swagger/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/error/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/favicon.ico").permitAll()
+                        // swagger
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling((exceptionConfig) ->
-                        exceptionConfig.authenticationEntryPoint(
-                                ((request, response, authException) -> {
-                                    BasicResponse b_response = BasicResponse.builder()
-                                            .success(false)
-                                            .message(Optional.of("인증되지 않은 사용자 입니다."))
-                                            .data(Optional.empty())
-                                            .build();
-                                    response.setContentType("application/json");
-                                    response.setStatus(401);
-                                    response.getWriter().write(b_response.toJson());
-                                })
-                        ).accessDeniedHandler(((request, response, accessDeniedException) -> {
-                            BasicResponse b_response = BasicResponse.builder()
-                                    .success(false)
-                                    .message(Optional.of("권한이 없는 사용자입니다."))
-                                    .data(Optional.empty())
-                                    .build();
-                            response.setStatus(403);
-                            response.setCharacterEncoding("utf-8");
-                            response.setContentType("application/json");
-                            response.getWriter().write(b_response.toJson());
-                        }))
-                );
         return http.build();
     }
 }
