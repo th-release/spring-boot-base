551aa47689677f108bb43c1515ff94aeca711370
cth.release <84012697+th-release@users.noreply.github.com>
2024-05-17 11:36:09 +0900

    feat: Added Cors Settings

---
+++ src/main/java/com/threlease/base/utils/security/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.threlease.base.utils.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
+    }
+}
