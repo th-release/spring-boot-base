d9ecd7e4e610d6e5f48270f38a3a0ff1567e3fde
cth.release <cth.release@gmail.com>
2026-04-07 08:07:53 +0900

    feat: 고아파일 삭제 기능 추가

--- src/main/java/com/threlease/base/BaseApplication.java
+++ src/main/java/com/threlease/base/BaseApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.security.SecureRandom;
 import java.util.Base64;
 
+
 @EnableCaching
+@EnableScheduling
 @EnableJpaRepositories
 @SpringBootApplication
 public class BaseApplication {
