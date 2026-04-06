971662f4ef0c1c7372f1c74e4cb31b642b187e90
cth.release <cth.release@gmail.com>
2026-04-06 12:38:46 +0900

    fix: usermanager

--- src/main/java/com/threlease/base/common/configs/WebSecurityConfig.java
+++ src/main/java/com/threlease/base/common/configs/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.threlease.base.common.configs;
 
-import com.threlease.base.common.provider.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +7,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // 비어있는 유저 매니저를 등록하여 기본 계정 생성을 방지함 (JWT 기반이므로)
+        return new InMemoryUserDetailsManager();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -22,6 +36,9 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(management ->
                         management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // 현재 모든 요청 허용 (JWT 검증은 인터셉터에서 수행)
                 );
         return http.build();
     }
