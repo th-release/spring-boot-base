008e455ee08a61e4673b521cfece69f57e9254aa
cth.release <cth.release@gmail.com>
2026-04-07 13:31:34 +0900

    style: cleanUpProperties 설정

--- src/main/java/com/threlease/base/common/properties/storage/cleanup/CleanupProperties.java
+++ src/main/java/com/threlease/base/common/properties/storage/cleanup/CleanupProperties.java
@@ -2,11 +2,15 @@ package com.threlease.base.common.properties.storage.cleanup;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ConfigurationProperties("storage.cleanup")
 @Getter
 @Setter
 public class CleanupProperties {
-    private String cron = "0 0 3 * * *";
-    private Long fixedRate = 0L;
-    private Integer chunkSize = 100;
+    private String cron;
+    private Long fixedRate;
+    private Integer chunkSize;
 }
